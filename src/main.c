382bf547bb09ab4356d776ce8130dfc4cee26877
Nekrofage <lesanglierdesardennes@gmail.com>
2024-08-24 16:00:53 +0200

    [Update] Licence header

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////////
 //  This file is part of Sorcerers. Copyright (C) 2020 @salvakantero
-//
+//                                                2024 @HackTechDev
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
